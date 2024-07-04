4b48aeab382739bdde26828ac4e8a78afc6fb6e7
Renegatto <alexey@mlabs.city>
2024-07-05 02:54:19 +0300

    Turtle -> Walker

--- src/IV_Datatypes/I_EnumerationTypes.hs
+++ src/IV_Datatypes/I_EnumerationTypes.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE DerivingStrategies #-}
 module IV_Datatypes.I_EnumerationTypes where
 
--- | A turtle, it has
-data Turtle = MkTurle
+-- | A Walker, it has
+data Walker = MkWalker
   (Int,Int) -- a position on XY plane
   Fuel -- a fuel level
   Facing -- a facing
@@ -10,7 +10,7 @@ data Turtle = MkTurle
 newtype Fuel = MkFuel Int
   deriving (Num, Show, Eq, Ord)
 
-data TurtleCommand
+data WalkerCommand
   = TurnLeft
   | TurnRight
   | Forward
@@ -18,11 +18,12 @@ data TurtleCommand
   | DoNothing
   deriving (Eq, Ord, Enum, Show)
 
-data TurtleMessage
+data WalkerMessage
   = IAmFacingObstacle
   | NotEnoughFuel
   | Done
   deriving (Eq, Ord, Enum, Show)
+
 data Facing = N | E | S | W -- clockwise
   deriving (Eq, Show, Enum)
 
@@ -35,42 +36,36 @@ fuelPerRefuel :: Fuel
 fuelPerRefuel = 5
 
 -- | Based on the current position and a message after previous step giving a command that
--- closing a turtle to the specified target.
+-- closing a Walker to the specified target.
 -- Note: obstacles do not need to be resolved completely, since this is not possible.
 advanceToTheTarget ::
   (Int,Int) ->
-  Fuel ->
-  (Int,Int) ->
-  Facing ->
-  TurtleMessage ->
-  TurtleCommand
-advanceToTheTarget targetPosition currentFuel currentPosition facing message = undefined
+  Walker ->
+  WalkerMessage ->
+  WalkerCommand
+advanceToTheTarget targetPosition walker message = undefined
 
 -- | Based on the starting point and a sequence of commands predicts
--- where the turtle will end up following them.
+-- where the Walker will end up following them.
 predictDestination ::
-  Fuel ->
-  (Int,Int) ->
-  Facing ->
-  [TurtleCommand] ->
+  Walker ->
+  [WalkerCommand] ->
   (Int,Int)
-predictDestination startingFuel startingPosition startingFacing commands = undefined
+predictDestination startingWalker commands = undefined
 
 -- | Minimizes amount of commands such that the outcome is the same
 -- Requirements:
 -- 1. two actions in a row such that one cancel another should not take place.
 -- 2. there should be no action such that when executed does nothing
-optimize :: [TurtleCommand] -> [TurtleCommand]
+optimize :: [WalkerCommand] -> [WalkerCommand]
 optimize = undefined
 
--- | As a turtle, given current fuel level, current position, obstacles map
+-- | As a Walker, given current fuel level, current position, obstacles map
 -- and a command, respond with a corresponding message
 -- and state after attempting to complete the command
 respond ::
-  Fuel ->
-  (Int,Int) ->
-  Facing ->
+  Walker ->
   [(Int,Int)] ->
-  TurtleCommand ->
-  (Fuel, (Int,Int), [TurtleMessage])
-respond fuelLevel currentPosition currentFacing obstaclesMap command = undefined 
+  WalkerCommand ->
+  (Walker, [WalkerMessage])
+respond walker obstaclesMap command = undefined 
