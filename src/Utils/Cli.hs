1e199aff2c28f26eaa2a928dcf0a30521f213800
Renegatto <edittorr@mail.ru>
2024-07-08 12:52:15 +0300

    Make tools to draw walker

---
+++ src/Utils/Cli.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE BlockArguments #-}
+module Utils.Cli where
+import Prelude hiding (putStrLn)
+import System.Console.ANSI qualified as ANSI
+import Data.Foldable (traverse_)
+
+drawThing :: [((Int,Int),String)] -> IO ()
+drawThing = traverse_ \((x,y),s) -> do
+  Just (height, _) <- ANSI.getTerminalSize
+  ANSI.setCursorPosition (height - y) x
+  -- print height
+  putStr s
+
+lineUp :: (Int,Int) -> Int -> ([(Int,Int)],(Int,Int))
+lineUp (x0,y0) len = ((x0,) <$> [min y0 end..max y0 end],(x0,end))
+  where end = y0 + len
+
+lineRight :: (Int,Int) -> Int -> ([(Int,Int)],(Int,Int))
+lineRight (x0,y0) len = ((,y0) <$> [min x0 end..max x0 end],(end,y0))
+  where end = x0 + len
+
+square :: (Int,Int) -> Int -> Int -> [(Int,Int)]
+square from height width =
+  let
+    (vert0, a) = lineUp from height
+    (hor0, b) = lineRight from width
+    (vert1, _) = lineUp b height
+    (hor1, _) = lineRight a width
+  in vert0 <> hor0 <> vert1 <> hor1
+
+squareToDraw = (,"o") <$> square (0,0) 5 10
+
+obstacles = [(3,2),(1,5),(3,3),(2,1),(3,4)]
+
+draw2 :: IO ()
+draw2 = do
+  ANSI.clearScreen
+  ANSI.setCursorPosition 0 0
+  putStr "O"
+  drawThing
+    [ ((0,0),"00")
+    , ((10,0),"10")
+    , ((0,10),"01")
+    , ((10,10),"11")
+    ]
+
+draw :: IO ()
+draw = do
+  Just (_,y) <- ANSI.getTerminalSize
+  ANSI.saveCursor
+  _ <- ANSI.getCursorPosition
+  ANSI.cursorUp 1
+  ANSI.setSGR [ANSI.SetColor ANSI.Foreground ANSI.Vivid ANSI.Blue]
+  ANSI.cursorUp 1
+  print =<< ANSI.getCursorPosition
+  ANSI.clearScreen
+  ANSI.restoreCursor
+  drawThing squareToDraw
+  ANSI.restoreCursor
+  drawThing [((0,0),("x"))]
+  ANSI.restoreCursor
+  drawThing $ (,"ж") <$> obstacles
+  ANSI.setCursorPosition y 0
